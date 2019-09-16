361d6e4250713fa1450ae065ed30e8d6a175b082
Carla Holcomb <carla.programmer@gmail.com>
2019-09-16 00:07:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
+def fizzbuzz(number)
+  if number%5 && number%3
+    puts fizzbuzz
+  elsif number%3
+    puts fizz
+  else 
+    puts buzz
+    
 
 end
